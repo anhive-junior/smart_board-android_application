6934c5115cae054bb147f18d4143634d7e16ecff
hurjm1004 <hurjm1004@naver.com>
2019-07-22 17:41:45 +0900

    glide

--- upload/app/src/main/java/com/example/upload/ShowImage.java
+++ upload/app/src/main/java/com/example/upload/ShowImage.java
@@ -4,15 +4,22 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,7 +49,7 @@ public class ShowImage extends AppCompatActivity implements Button.OnClickListen
 
         UPLOAD_URL = ((GlobalVar)this.getApplication()).getMyAddr() + "/signage/s00_signage.php";
         Intent intent = getIntent();
-        showimage(intent.getExtras().getString("filepath"), Boolean.FALSE);
+        //showimage(intent.getExtras().getString("filepath"), Boolean.FALSE);
 
 
         imageView = (ImageView) findViewById(R.id.imageView3);
@@ -52,6 +59,25 @@ public class ShowImage extends AppCompatActivity implements Button.OnClickListen
 
         buttonBack.setOnClickListener(this);
         buttonDelete.setOnClickListener(this);
+
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
+
+        Glide.with(getApplicationContext())
+                .load("http://" + intent.getExtras().getString("filepath"))
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                .into(imageView);
     }
 
     @Override
