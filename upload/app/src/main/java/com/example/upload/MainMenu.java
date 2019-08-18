a4c253a6fcb4a200f0929fa3c20ae3438b847291
hurjm1004 <hurjm1004@naver.com>
2019-08-19 00:11:23 +0900

    UI

--- upload/app/src/main/java/com/example/upload/MainMenu.java
+++ upload/app/src/main/java/com/example/upload/MainMenu.java
@@ -2,10 +2,13 @@ package com.example.upload;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.upload.Camera.CameraActivity;
@@ -18,8 +21,8 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
         private ImageButton buttonList;
         private ImageButton buttonSlide;
         private ImageButton buttonCamera;;
-        private Button buttonUsersetting;
-        private Button buttonSetting;
+        private ImageButton buttonUsersetting;
+        private ImageButton buttonSetting;
         private Intent intent;
 
 
@@ -28,19 +31,17 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             super.onCreate(savedInstanceState);
             setContentView(R.layout.mainmenu);
 
-            buttonUsersetting = (Button) findViewById(R.id.usersetting);
-            buttonSetting = (Button) findViewById(R.id.setting);
             buttonCamera = (ImageButton) findViewById(R.id.button_camera);
             buttonUpload = (ImageButton) findViewById(R.id.button_upload);
             buttonList = (ImageButton) findViewById(R.id.button_list);
             buttonSlide = (ImageButton) findViewById(R.id.button_slide);
 
-            buttonUsersetting.setOnClickListener(this);
-            buttonSetting.setOnClickListener(this);
             buttonCamera.setOnClickListener(this);
             buttonUpload.setOnClickListener(this);
             buttonList.setOnClickListener(this);
             buttonSlide.setOnClickListener(this);
+
+            setActionBar();
         }
 
     @Override
@@ -58,14 +59,45 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             case R.id.button_slide :
                 intent = new Intent(getApplicationContext(), ManageSlide.class);
                 break;
-            case R.id.usersetting :
-                intent = new Intent(getApplicationContext(), UserSetting.class);
-                break ;
-            case R.id.setting :
-                intent = new Intent(getApplicationContext(), PlayWork.class);
-                break ;
         }
         startActivity(intent);
+    }
+
+    private void setActionBar(){
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.actionbar, null);
+        actionBar.setCustomView(mCustomView);
+
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(mCustomView, params);
+
+        Toolbar parent = (Toolbar) mCustomView.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+
+        buttonSetting = (ImageButton) findViewById(R.id.setting);
+        buttonUsersetting = (ImageButton) findViewById(R.id.usersetting);
+
+        buttonSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               intent = new Intent(getApplicationContext(), PlayWork.class);
+               startActivity(intent);
+            }
+        });
+
+        buttonUsersetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), UserSetting.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
